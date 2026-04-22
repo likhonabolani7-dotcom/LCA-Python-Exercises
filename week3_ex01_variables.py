ba936ff96a36375cc378a63c1eab8deee12197fe
likhonabolani7-dotcom <likhonabolani7@gmail.com>
2026-04-22 06:13:45 -0700

    Add files via upload

---
+++ week3_ex01_variables.py
@@ -0,0 +1,13 @@
+name = "LIKHONA"
+age = 25
+print(f"Hello {name} you are {age} years old")
+
+length = 10
+width = 5 
+area = length * width
+print(f"The area of the rectangle is {area}")
+
+celsius = 30
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit")
+
